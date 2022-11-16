f7f5ab65ed750c3d6795479d7961c607b3add003
fbrightw <gasanova_1998@bk.ru>
2022-11-16 13:50:02 +0300

    cards changed

--- src/components/Cards.tsx
+++ src/components/Cards.tsx
@@ -30,15 +30,18 @@ export default function Cards() {
 
     return(
         <Card style={{ width: '18rem', margin: '5% auto'}}>
-            <Card.Img variant="top" src="holder.js/100px180" />
-            <Card.Body>
-                <Card.Title>Card Title</Card.Title>
-                <Card.Text>
-                    Some quick example text to build on the card title and make up the
-                    bulk of the card's content.
-                </Card.Text>
-                <Button variant="primary">Go somewhere</Button>
-            </Card.Body>
+            {(breed) &&
+                <>
+                  <Card.Img variant="top" src="holder.js/100px180"/><Card.Body>
+                    <Card.Title>{breed.name}</Card.Title>
+                      <Card.Text>
+                        Some quick example text to build on the card title and make up the
+                        bulk of the card's content.
+                      </Card.Text>
+                    <Button variant="primary">Go somewhere</Button>
+                  </Card.Body>
+                </>
+            }
         </Card>
     )
 }
