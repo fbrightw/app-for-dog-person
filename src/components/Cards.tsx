6f0b17eb0c74b023350e90f25e10ec8f71a92bd2
fbrightw <gasanova_1998@bk.ru>
2022-11-26 21:09:30 +0300

    added arrow onclick which scrolled up to top

--- src/components/Cards.tsx
+++ src/components/Cards.tsx
@@ -1,5 +1,6 @@
 import React, {useEffect, useState} from 'react';
 import {Card, ListGroup} from "react-bootstrap";
+import {wishList} from "../utils";
 
 interface IBreedInfo {
   weight: {
@@ -43,6 +44,7 @@ export default function Cards(props: ISelectedBreed) {
         .then(data => {
               setBreedInfo(data)
               loadImage(data)
+              setIsLiked(false)
             }
         )
   }, [props.selectedBreed])
@@ -56,32 +58,53 @@ export default function Cards(props: ISelectedBreed) {
         })
   }
 
-  function onLikeClick(value: boolean) {
-      console.log('click', value)
-      // @ts-ignore
-      document.getElementById("heart").className += 'red-color'
-      setIsLiked(value)
+  function onLikeClick() {
+      setIsLiked(!isLiked)
+      // if (isLiked)
+      //     wishList.push(props.selectedBreed)
+      // else
+      //     wishList.filter(
+      //         el !== {props.selectedBreed}
+      //     )
   }
+
+  function onArrowClick() {
+      // @ts-ignore
+      window.scrollTo({
+          top: 0,
+          behaviour: 'smooth'
+      })
+    }
 
     return (
         (props.selectedBreed !== '' && breedInfo.length > 0) ?
-            <Card style={{flexDirection: 'row', alignItems: 'center', padding: '10px', margin: '5% auto', width: '80%'}}>
-                <Card.Body>
-                    <Card.Title>{breedInfo[0].name}</Card.Title>
-                    <ListGroup variant="flush">
-                        <ListGroup.Item>{'height: '}{breedInfo[0].height.imperial}</ListGroup.Item>
-                        <ListGroup.Item>{'weight: '}{breedInfo[0].weight.imperial}</ListGroup.Item>
-                        <ListGroup.Item>{'temperament: '}{breedInfo[0].temperament}</ListGroup.Item>
-                        <ListGroup.Item>{'life span: '}{breedInfo[0].life_span}</ListGroup.Item>
-                    </ListGroup>
-                    <button type="button" className="btn btn-light" onClick={() => setIsLiked(!isLiked)}>
-                        <span id="heart" className={isLiked ? "bi bi-heart-fill red-color" : "bi bi-heart-fill"}></span>
-                    </button>
-                </Card.Body>
-                <div style={{width: '30%'}}>
-                    <img src={breedImg.url}  alt="b" style={{borderRadius: '2%'}}/>
-                </div>
-            </Card>
+            <div style={{display: 'flex', flexWrap: 'wrap', alignContent: 'center'}}>
+                <button className="back-to-top" style={{margin: '0 auto'}} onClick={() => onArrowClick()}>
+                    <i className="bi bi-arrow-up-circle" style={{fontSize: '50px'}}></i>
+                </button>
+                <Card style={{flexDirection: 'row', alignItems: 'center', padding: '10px', margin: '5% auto', width: '80%', float: 'right'}}>
+                    <Card.Body>
+                        <Card.Title>{breedInfo[0].name}</Card.Title>
+                        <ListGroup variant="flush">
+                            <ListGroup.Item>{'height: '}{breedInfo[0].height.imperial}</ListGroup.Item>
+                            <ListGroup.Item>{'weight: '}{breedInfo[0].weight.imperial}</ListGroup.Item>
+                            <ListGroup.Item>{'temperament: '}{breedInfo[0].temperament}</ListGroup.Item>
+                            <ListGroup.Item>{'life span: '}{breedInfo[0].life_span}</ListGroup.Item>
+                        </ListGroup>
+                        <button
+                            type="button"
+                            className="btn btn-light"
+                            onClick={() => onLikeClick()}
+                        >
+                            <span id="heart" className={isLiked ? "bi bi-heart-fill red-color" : "bi bi-heart-fill"}></span>
+                        </button>
+                    </Card.Body>
+                    <div style={{width: '30%'}}>
+                        <img src={breedImg.url}  alt="b" style={{borderRadius: '2%'}}/>
+                    </div>
+                </Card>
+            </div>
             : null
+
     );
 }
