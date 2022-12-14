fa3a2f6dd54077e6514519f602ec04fb82ba24b0
fbrightw <gasanova_1998@bk.ru>
2022-12-14 22:08:30 +0300

    rendering component with express

--- backend/controllers/liked_list.js
+++ backend/controllers/liked_list.js
@@ -1,7 +1,11 @@
-function liked_list(request, response) {
-  //need to check if there is data in bd, if not write corresponding answer
+const path = require('path');
 
-  return response.send("data from bd")
+
+function liked_list(req, res) {
+  //need to check if there is data in bd, if not write corresponding answer
+  // res.sendFile(path.join(__dirname, "../../public", "index.html"));
+  // return response.send("data from bd")
+  res.render('index')
 }
 
 module.exports = {liked_list}
